3401fd6f745cc61f4e94076e7cb8afc46b5f3aa3
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-11-11 15:12:22 +0100

    feta: Update Flutter build process and add Google Drive authentication

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,37 @@
 use std::process::Command;
 use std::{error::Error, path::Path};
-use std::{io, process};
+use std::{io, process, fs};
 
 pub use config::Config;
 
 mod config;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Checking if in a flutter project...");
+    println!("Checking if in a Flutter project...");
 
     let path: &str = config.path.as_str();
     check_if_in_flutter_project(path).unwrap_or_else(|err| {
-        eprintln!("Error: Not a flutter project\n{}", err);
+        eprintln!("Error: Not a Flutter project\n{}", err);
         process::exit(1);
     });
 
-    println!("flutter project found...");
-    println!("🚀 Running flutter build...");
-    run_flutter_build(config).unwrap_or_else(|err| {
-        eprintln!("⚠️ Error: Failed to run flutter build..\n{}", err);
+    println!("Flutter project found...");
+    println!("🚀 Running Flutter build...");
+    run_flutter_build(&config).unwrap_or_else(|err| {
+        eprintln!("⚠️ Error: Failed to run Flutter build..\n{}", err);
         process::exit(1);
     });
+
+    println!("\n✅ Flutter build completed...");
+    println!("Opening apk path...");
+    open_file_path(path)?;
+
+    println!("\nGetting File");
+    let full_path = format!("{}/build/app/outputs/flutter-apk/app-release.apk", path);
+    let file = fs::read(full_path)?;
+    println!("{:?}", String::from_utf8_lossy(&file));
+
+
     Ok(())
 }
 
@@ -38,15 +49,23 @@ fn check_if_in_flutter_project(path: &str) -> io::Result<()> {
     }
 }
 
-fn run_flutter_build(config: Config) -> Result<(), Box<dyn Error>> {
+fn run_flutter_build(config: &Config) -> Result<(), Box<dyn Error>> {
     let mut run_flutter = Command::new("flutter")
-        .current_dir(config.path)
+        .current_dir(config.path.clone())
         .arg("build")
         .arg("apk")
         .spawn()?;
 
-    println!("{:?}", run_flutter);
     run_flutter.wait()?;
+    Ok(())
+}
 
+fn open_file_path(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut run_open_path = Command::new("open")
+        .current_dir(path)
+        .arg("./build/app/outputs/flutter-apk/")
+        .spawn()?;
+
+    run_open_path.wait()?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,49 @@
-use std::process;
+// use std::process;
 
-use hecktor::*;
+use std::error::Error;
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("-------- Problem parsing arguments -------- \n{}", err);
-        process::exit(1);
-    });
+use google_drive3::oauth2;
+// use hecktor::*;
 
-    if let Err(err) = run(config) {
-        eprintln!("Application stop due to error: {}", err);
-        process::exit(1);
-    }
+// async fn run() -> Result<(), Box<dyn Error>> {
+// let args: Vec<String> = std::env::args().collect();
+// let config = Config::new(&args).unwrap_or_else(|err| {
+//     eprintln!("-------- Problem parsing arguments -------- \n{}", err);
+//     process::exit(1);
+// });
+// if let Err(err) = run(config) {
+//     eprintln!("Application stop due to error: {}", err);
+//     process::exit(1);
+// }
+// Ok(())
+// }
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    authenticate_with_google_drive().await?;
+
+    Ok(())
+}
+
+//
+async fn authenticate_with_google_drive() -> Result<(), Box<dyn std::error::Error>> {
+    // Load your OAuth 2.0 credentials from a file
+    let secret = oauth2::read_application_secret("client_secret.json").await?;
+
+    // Create an authenticator that uses the client secret to authenticate
+    let auth = oauth2::InstalledFlowAuthenticator::builder(
+        secret,
+        oauth2::InstalledFlowReturnMethod::HTTPRedirect,
+    )
+    .build()
+    .await?;
+
+    // Request access to Google Drive
+    let token = auth
+        .token(&["https://www.googleapis.com/auth/drive.file"])
+        .await?;
+
+    // Return the access token
+    println!("{:?}", token);
+    Ok(())
 }
